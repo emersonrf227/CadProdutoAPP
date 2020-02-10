3732573c25693e6e69187edfe83fcef4cf2dd3e4
emersonrodrigues <emersonrf227@yahoo.com.br>
2020-02-10 15:50:38 -0300

    newPush rits

--- src/pages/caditem.js
+++ src/pages/caditem.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, TextInput, View, SafeAreaView, Image } from 'react-native'
+import { StyleSheet, TextInput, View, SafeAreaView, Image,  } from 'react-native'
 import { Container, Header, Button, Icon, Text, Input, Item, Content, Form, FooterTab, Footer, Textarea } from 'native-base';
+import { Rating, AirbnbRating } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 export default class Caditem extends React.Component {
 
     static navigationOptions = { title: "Cadastro de  Produtos" }
     state = { codB: '', nProduto: '', valProduto: '', desItem: '', errorMessage: null };
+   
 
 
 
@@ -40,19 +43,25 @@ export default class Caditem extends React.Component {
            var dados = JSON.stringify(arrayProd);
             alert('Produto Salvo '+ dados);
 
-
-
-
-
-
         }
-
-
     };
+        ratingCompleted(rating) {
+            console.log("Rating is: " + rating)
+          }
+
+
+
     render() {
+
+        const WATER_IMAGE = require('../../assets/img/water.png')
+ 
+
         return (
+            
             <SafeAreaView>
                 <Text style={{ fontSize: 30, textAlign: "center" }}>Cadastro de Produtos </Text>
+
+                <ScrollView>
                 <Form>
                     <Item floatingLabel>
                         <Input placeholder="Código de Barras"
@@ -74,16 +83,28 @@ export default class Caditem extends React.Component {
                             value={this.state.valProduto}
                         />
                     </Item>
-                    <Textarea style={{ marginTop: 30 }} floatingLabel last rowSpan={5} bordered placeholder="Descriçao do Produto"
+                    <Textarea style={{ marginTop: 20 }} floatingLabel last rowSpan={5} bordered placeholder="Descriçao do Produto"
                         onChangeText={desItem => this.setState({ desItem })}
                         value={this.state.desItem}
                     />
+                     
+ <AirbnbRating
+   count={5}
+   reviews={["Péssimo", "Ruim", "Normal", "Bom", "Excelente"]}
+   defaultRating={0}
+   size={20}
+ />
 
-                    <Button onPress={this.salvaItem} full success>
+                    <Button style={{marginTop:10}} onPress={this.salvaItem} full success>
                         <Text > Cadastra Item  </Text>
                     </Button>
 
                 </Form>
+
+                </ScrollView>
+
+  
+ 
 
 
             </SafeAreaView>
